2cd857c4dd9de8e9e38d695aa7d6220b19dd7b03
Jacob Mason <jm2232@msstate.edu>
2013-10-09 16:01:38 -0500

    More development. Classes are in card_logic.py

--- War with Classes.py
+++ War with Classes.py
@@ -1,161 +1,58 @@
-import random
-import time
-
-class Deck:    
-    def __init__(self):
-        self.cards = []
-        self.acesHigh = (input("Are Aces high? (y/n) ") == "y")
-        for x in range(1, 5):
-            if self.acesHigh:
-                for y in range(2, 15):
-                    self.cards.append((x, y))
-            else:
-                for y in range(1, 14):
-                    self.cards.append((x, y))
-                    
-    def shuffle(self, userList):
-        i = 0
-        while self.cards != []:
-            for user in userList:
-                if self.cards != []:
-                    poppedCard = self.cards.pop(random.randint(0, 51 - i))
-                    user.Deck.append(poppedCard)
-                    i += 1
-        return
-
-        
-class Hand:
-    def __init__(self, owner):
-        self.Deck = []
-        self.Card = ()
-        self.owner = owner    
-        
-    def flip_card(self):
-        return self.Deck.pop(0)
-
-class Card:
-    def __init__(self, cardTuple):
-        self.cardTuple = cardTuple
-        self.suit = cardTuple[0]
-        self.rank = cardTuple[1]
+from card_logic import *
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+  
+  
+  
+      
+Deck = Deck(True) #initialize deck. True means Aces are high
+userNameList = []
+while userNameList == []:
+    userNameList.append(input("Please enter your name: "))
     
-    def __str__(self):
-        
-        if self.suit==1:
-            suit = "Diamonds"        
-        elif self.suit==2:
-            suit = "Clubs"       
-        elif self.suit==3:
-            suit = "Hearts"
-        else:
-            suit = "Spades"  
-        
-        if self.rank == 1:
-            rank = "Ace"       
-        elif self.rank == 2:
-            rank = "Two"        
-        elif self.rank == 3:
-            rank = "Three"
-        elif self.rank == 4:
-            rank = "Four"
-        elif self.rank == 5:
-            rank = "Five"
-        elif self.rank == 6:
-            rank = "Six"
-        elif self.rank == 7:
-            rank = "Seven"
-        elif self.rank == 8:
-            rank = "Eight"
-        elif self.rank == 9:
-            rank = "Nine"    
-        elif self.rank == 10:
-            rank = "Ten"      
-        elif self.rank == 11:
-            rank = "Jack"      
-        elif self.rank == 12:
-            rank = "Queen"      
-        elif self.rank == 13:
-            rank = "King"       
-        else: # when cardTuple == 14 because of high Aces
-            rank = "Ace"       
-        
-        return (rank + " of " + suit)        
-        
-    def draw(self):
-        if self.suit==1:
-            pic3 = "| :/\: |"
-            pic4 = "| :\/: |"         
-        elif self.suit==2:
-            pic3 = "| :(): |"
-            pic4 = "| ()() |"        
-        elif self.suit==3:
-            pic3 = "| (\/) |"
-            pic4 = "| :\/: |"        
-        else:
-            pic3 = "| :/\: |"
-            pic4 = "| (__) |"
-            
-        if self.rank > 1 and self.rank < 10:
-            pic2 = str(self.rank) + "."
-            pic5 = "'" + str(self.rank)
-        elif self.rank == 10:    
-            pic2 = str(self.rank)
-            pic5 = str(self.rank)
-        elif self.rank == 11:
-            pic2 = "J."
-            pic5 = "'J"
-        elif self.rank == 12:
-            pic2 = "Q."
-            pic5 = "'Q"         
-        elif self.rank == 13:
-            pic2 = "K."
-            pic5 = "'K" 
-        else:
-            pic2 = "A."
-            pic5 = "'A"
+PCnum = int(input("How many Computer opponents would you like to compete against? "))
+while PCnum <=0:
+    print("You must compete against at least one Computer.")
+    PCnum = int(input("How many Computer opponents would you like to compete against? "))
     
-        picture = [".------.", "|" + pic2 + "--. |", pic3, pic4, "| '--" + pic5 + "|", "`------'"]
-        
-        for line in picture:
-            print(line)
-        
-        return  
+for i in range(0, PCnum):
+    userNameList.append("Computer " + str(i+1))  #i is incremented because Computer_0 sounds stupid
+
+# users contains the list of users that will be playing, including PCs
+# every user in the list will be turned into an object of class Hand
+users = []
+for userName in userNameList:
+    users.append(Hand(userName)) #initialize hands. The string will represent the name of the winner.
+
+Deck.shuffle(users) #shuffle the deck and deal to all players
+
+
+while("playcontinues" == "playcontinues"):
+    for i in users: #i represents a single user. This loop will iterate through all users playing
+        log.debug("%s: %s Cards: %s", i.owner, len(i.Deck), i.Deck)
     
-    def compare(self, otherCard):
-        if self.rank > otherCard.rank:
-            print("\nYou are the winner!")
-            winner = self.owner
-        elif self.rank < otherCard.rank:
-            print("\nThe Computer is the winner!")
-            winner = otherCard.owner
-        else:
-            print("\nThese cards are equal: It's a tie!")
-            time.sleep(2)
-            input("\nPress the enter key to proceed with DOUBLE WAR!")
-            winner = "Tie"
-            
-        time.sleep(.5)
+    card = []
+    for i in range(0, len(users)):
+        log.debug("Iteration: %i", i)
+        log.debug("User: %s", users[i].owner)
+        card.append( Card(users[i].flip_card()) )
+        log.debug("Card: %s", str(card[i])) 
+    
+    for i in range(1, len(users)):
+        print(card[i], "\t", end="")
         
-        return winner        
+    print()
+    for i in range(1, len(users)):
+        card[i].draw()
         
-
-Deck = Deck() #initialize deck
-Player, PC = Hand("Player"), Hand("PC") #initialize hands. The string will represent the name of the winner.
-Deck.shuffle([Player, PC]) #shuffle the deck to both players
-
-print(Player.Deck)
-print(PC.Deck)
-
-print(len(Player.Deck))
-print(len(PC.Deck))
-
-Player.Card = Player.flip_card()  #card1 and card2 are tuples
-PC.Card = PC.flip_card()
-
-print(playerCard)
-playerCard.draw()
-print(PCCard)
-PCCard.draw()
-
-winner = playerCard.compare(PCCard)
-print(winner)
+    print()
+    card[0].draw()
+    print(card[0])
+    print()
+    
+    winner = card[0].compare(card[1])
+    
+    
+    
+    input("Press enter key to continue...")
---
+++ card_logic.py
@@ -0,0 +1,151 @@
+# card_logic.py
+
+import random
+import time
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+class Deck:    
+    def __init__(self, acesHigh):
+        self.cards = []
+        # The old way to assign cards was to let the user decide if Aces were to be high.
+        # I'm keeping the method listed so I can add it back later when I make a configuration menu.
+        # self.acesHigh = (input("Are Aces high? (y/n) ") == "y")
+        self.acesHigh = acesHigh
+        for x in range(1, 5):
+            if self.acesHigh:
+                log.debug("Aces are high")
+                for y in range(2, 15):
+                    self.cards.append((x, y))
+            else:
+                log.debug("Aces are low")
+                for y in range(1, 14):
+                    self.cards.append((x, y))
+                    
+    def shuffle(self, userList):
+        i = 0
+        while self.cards != []:
+            for user in userList:
+                if self.cards != []:
+                    poppedCard = self.cards.pop(random.randint(0, 51 - i))
+                    user.Deck.append(poppedCard)
+                    i += 1
+        return
+
+        
+class Hand:
+    def __init__(self, owner):
+        self.Deck = []
+        self.Card = ()
+        # Calling .owner on a Hand will return the username as a string
+        self.owner = owner    
+        
+    def flip_card(self):
+        #returns the card and the owner:  [(1,2), "Player"]
+        return [self.Deck.pop(0), self.owner]  
+    
+class Card:
+    def __init__(self, cardList):
+        self.cardTuple = cardList[0]
+        self.suit = self.cardTuple[0]
+        self.rank = self.cardTuple[1] 
+        self.owner = cardList[1]
+        
+    def __str__(self):      
+        if self.suit==1:
+            suit = "Diamonds"        
+        elif self.suit==2:
+            suit = "Clubs"       
+        elif self.suit==3:
+            suit = "Hearts"
+        else:
+            suit = "Spades"  
+        
+        if self.rank == 1:
+            rank = "Ace"       
+        elif self.rank == 2:
+            rank = "Two"        
+        elif self.rank == 3:
+            rank = "Three"
+        elif self.rank == 4:
+            rank = "Four"
+        elif self.rank == 5:
+            rank = "Five"
+        elif self.rank == 6:
+            rank = "Six"
+        elif self.rank == 7:
+            rank = "Seven"
+        elif self.rank == 8:
+            rank = "Eight"
+        elif self.rank == 9:
+            rank = "Nine"    
+        elif self.rank == 10:
+            rank = "Ten"      
+        elif self.rank == 11:
+            rank = "Jack"      
+        elif self.rank == 12:
+            rank = "Queen"      
+        elif self.rank == 13:
+            rank = "King"       
+        else: # when cardTuple == 14 because of high Aces
+            rank = "Ace"       
+        
+        return (rank + " of " + suit)        
+        
+    def draw(self):
+        if self.suit==1:
+            pic3 = "| :/\: |"
+            pic4 = "| :\/: |"         
+        elif self.suit==2:
+            pic3 = "| :(): |"
+            pic4 = "| ()() |"        
+        elif self.suit==3:
+            pic3 = "| (\/) |"
+            pic4 = "| :\/: |"        
+        else:
+            pic3 = "| :/\: |"
+            pic4 = "| (__) |"
+            
+        if self.rank > 1 and self.rank < 10:
+            pic2 = str(self.rank) + "."
+            pic5 = "'" + str(self.rank)
+        elif self.rank == 10:    
+            pic2 = str(self.rank)
+            pic5 = str(self.rank)
+        elif self.rank == 11:
+            pic2 = "J."
+            pic5 = "'J"
+        elif self.rank == 12:
+            pic2 = "Q."
+            pic5 = "'Q"         
+        elif self.rank == 13:
+            pic2 = "K."
+            pic5 = "'K" 
+        else:
+            pic2 = "A."
+            pic5 = "'A"
+    
+        picture = [".------.", "|" + pic2 + "--. |", pic3, pic4, "| '--" + pic5 + "|", "`------'"]
+        
+        for line in picture:
+            print(line)
+        
+        return      
+    
+    def compare(self, otherCard):
+        if self.rank > otherCard.rank:
+            print(self.owner, "is the winner!")
+            winner = self.owner
+        elif self.rank < otherCard.rank:
+            print(otherCard.owner, "is the winner!")
+            winner = otherCard.owner
+        else:
+            print("\nThese cards are equal: It's a tie!")
+            time.sleep(2)
+            input("\nPress the enter key to proceed with DOUBLE WAR!")
+            winner = "Tie"
+            
+        time.sleep(.5)
+        
+        return winner
